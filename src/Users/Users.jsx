1813c84b885e070bbdd7f0109fe6297cb32e1f0f
Dan Korzh <lienglex@gmail.com>
2020-10-13 19:15:13 +0300

    add: render received users

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react'
 import {getUsersActionCreator} from "../Store/actions/getUsers";
 import {connect} from 'react-redux'
+import {List} from "antd";
 
 const mapStateToProps = (state) => ({
     users: state.getUsersReducer.users
@@ -19,7 +20,29 @@ class Users extends PureComponent {
     }
 
     render() {
-        return ()
+        return (
+            <List
+                // itemLayout is layout of list
+                itemLayout='horizontal'
+                header="All Users"
+                // dataSource is array for list
+                dataSource={Object.values(this.props.users)}
+                // adds border
+                bordered
+                size='small'
+                // renderItem uses elements from dataSource to render
+                renderItem={(user) => (
+                    <List.Item>
+                        <List.Item.Meta
+                            title={<a href="/">{user.name}</a>}
+                            description={user.email}
+                        />
+                    </List.Item>
+                )}
+                // additional class, used in `styles/main-container.scss`
+                className='users-container'
+            />
+        )
     }
 }
 
