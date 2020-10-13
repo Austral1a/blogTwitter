19993d6ef71341c15c0442c44a87edecc0abecfb
Dan Korzh <lienglex@gmail.com>
2020-10-13 19:33:56 +0300

    add: getAllPosts reducer

---
+++ src/Store/reducers/getAllPostsReducer.js
@@ -0,0 +1,20 @@
+import {GET_ALL_POSTS} from '.../actions/action-types'
+
+const initState = {
+    posts: {}
+}
+
+const getAllPostsReducer = (state = initState, action) => {
+    switch (action.type) {
+        case GET_ALL_POSTS:
+            return Object.assign({}, state, {
+                // action.posts gets its value from called corresponding
+                // action creator which in its turn invokes an action(GET_ALL_POSTS to the right of the `case` operator)
+                posts: action.posts
+            })
+        default:
+            return state
+    }
+}
+
+export default getAllPostsReducer
