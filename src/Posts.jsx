60b59cfe171a99d5febcec5b0fcbc30c41560066
Dan Korzh <lienglex@gmail.com>
2020-10-13 19:38:05 +0300

    add: Posts' mapStateToProps

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,5 +1,10 @@
 import React, {PureComponent} from 'react'
 
+const mapStateToProps = (state) => ({
+    posts: state.getAllPostsReducer.posts
+})
+
+
 class Posts extends PureComponent {
     render() {
         return('')
