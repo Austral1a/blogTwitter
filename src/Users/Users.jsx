2ef2ab7670d2d82ae364f4fd9b7a25473d3e4629
Dan Korzh <lienglex@gmail.com>
2020-10-13 16:17:14 +0300

    add: mapStateToProps with users

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,4 +1,9 @@
 import React, {PureComponent} from 'react'
+import {getUsersActionCreator} from "../Store/actions/getUsers";
+
+const mapStateToProps = (state) => ({
+    users: state.getUsersReducer.users
+})
 
 class Users extends PureComponent {
     render() {
