e5b71238a46d502e4aa67e7aa630b30a847aa3d8
Dan Korzh <lienglex@gmail.com>
2020-10-14 00:35:17 +0300

    add: added getPostCommentsReducer to rootReducer

--- src/Store/reducers/rootReducer.js
+++ src/Store/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import getUsersReducer from './getUsersReducer'
 import getAllPostsReducer from './getAllPostsReducer'
 import getUserPostsReducer from './getUserPostsReducer'
 import getCurrUserIdReducer from './getCurrUserIdReducer'
+import getPostCommentsReducer from './getPostCommentsReducer'
 
 export const rootReducer = combineReducers({
     getUsersReducer,
     getAllPostsReducer,
     getUserPostsReducer,
-    getCurrUserIdReducer
+    getCurrUserIdReducer,
+    getPostCommentsReducer
 })
