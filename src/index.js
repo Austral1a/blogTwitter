29495411993fe9d059aa02f528b6efdbc3a29519
Dan Korzh <lienglex@gmail.com>
2020-10-13 23:57:21 +0300

    add: routes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import ConnectedUsers from "./Users/Users";
 import ConnectedPosts from './Posts'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './Store/index'
 import './styles/general.scss'
 import './styles/main-container.scss'
+import ConnectedUserPagePosts from "./Pages/UserPagePosts";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-        <div className='main-container'>
-            <ConnectedUsers />
-            <ConnectedPosts />
-        </div>
+        <Router>
+            <div className='main-container'>
+                <ConnectedUsers />
+                <Switch>
+                    <Redirect exact from='/' to='/posts' />
+                    <Route exact path='/posts'>
+                        <ConnectedPosts />
+                    </Route>
+                    <Route exact path={`/users/:id/posts`}>
+                        <ConnectedUserPagePosts />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
