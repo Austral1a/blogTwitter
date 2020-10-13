95dcc9e026e120aea905f3fd8f75281f4f72c853
Dan Korzh <lienglex@gmail.com>
2020-10-13 16:36:13 +0300

    add: User's props

--- src/Users/User.jsx
+++ src/Users/User.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const User = () => {
+const User = ({
+                  username,
+                  name,
+                  phone,
+                  website
+              }) => {
     return ()
 }
 
