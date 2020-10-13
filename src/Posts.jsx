6a97adae0c6e56391083fa9cadf29555472336c8
Dan Korzh <lienglex@gmail.com>
2020-10-13 19:42:03 +0300

    add: Posts' mapDispatchToProps with getAllPosts action creator

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,9 +1,16 @@
 import React, {PureComponent} from 'react'
+import {connect} from 'react-redux'
+import getAllPostsActionCreator from "./Store/actions/getUsers";
 
 const mapStateToProps = (state) => ({
     posts: state.getAllPostsReducer.posts
 })
 
+const mapDispatchToProps = (dispatch) => ({
+    getAllPosts: () => {
+        dispatch(getAllPostsActionCreator())
+    }
+})
 
 class Posts extends PureComponent {
     render() {
