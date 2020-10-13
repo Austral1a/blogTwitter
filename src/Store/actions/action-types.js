ea7053546a713d35224703da731d153d973af723
Dan Korzh <lienglex@gmail.com>
2020-10-13 23:39:54 +0300

    add: GET_CURR_USER_ID action type

--- src/Store/actions/action-types.js
+++ src/Store/actions/action-types.js
@@ -1,3 +1,4 @@
 export const GET_USERS = 'GET_USERS'
 export const GET_ALL_POSTS = 'GET_ALL_POSTS'
 export const GET_USER_POSTS = 'GET_USER_POSTS'
+export const GET_CURR_USER_ID = 'GET_CURR_USER_ID'
