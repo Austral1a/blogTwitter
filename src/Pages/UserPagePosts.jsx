a1471ac2f9c29edb00087dc0f587fc3798b84829
Dan Korzh <lienglex@gmail.com>
2020-10-13 23:50:06 +0300

    add: mapStateToProps, mapDispatchToProps; HOC ConnectedUserPagePosts

--- src/Pages/UserPagePosts.jsx
+++ src/Pages/UserPagePosts.jsx
@@ -1,8 +1,27 @@
 import React, {PureComponent} from 'react'
+import {connect} from 'react-redux'
+import getUserPostsActionCreator from '../Store/actions/getUserPosts'
+
+const mapStateToProps = (state) => ({
+    posts: state.getUserPostsReducer.posts,
+    currUserId: state.getCurrUserIdReducer.userId
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    getUserPosts: (userId) => {
+        dispatch(getUserPostsActionCreator(userId))
+    }
+})
 
 class UserPagePosts extends PureComponent {
-
     render() {
         return ('')
     }
 }
+
+const ConnectedUserPagePosts = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(UserPagePosts)
+
+export default ConnectedUserPagePosts
