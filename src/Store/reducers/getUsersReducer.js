b78bd56ac2a9bde843c1dc7c1d0edf2540dd2e9c
Dan Korzh <lienglex@gmail.com>
2020-10-13 15:22:22 +0300

    add: getUsers reducer

---
+++ src/Store/reducers/getUsersReducer.js
@@ -0,0 +1,20 @@
+import {GET_USERS} from '../actions/action-types'
+
+const initState = {
+    users: {}
+}
+
+const getUsersReducer = (state = initState, action) => {
+    switch (action.type) {
+        case GET_USERS:
+            return Object.assign({}, state, {
+                // action.users gets its value from called corresponding
+                // action creator which in its turn invokes an action(GET_USERS to the right of the `case` operator)
+                users: action.users
+            })
+        default:
+            return state
+    }
+}
+
+export default getUsersReducer
