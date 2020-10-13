63353e2a0f9159d2203d325e5a6104ee1fc647ec
Dan Korzh <lienglex@gmail.com>
2020-10-13 23:41:38 +0300

    add: getUserPosts and getCurrUserId reducers

--- src/Store/reducers/rootReducer.js
+++ src/Store/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux'
 import getUsersReducer from './getUsersReducer'
 import getAllPostsReducer from './getAllPostsReducer'
+import getUserPostsReducer from './getUserPostsReducer'
+import getCurrUserIdReducer from './getCurrUserIdReducer'
 
 export const rootReducer = combineReducers({
     getUsersReducer,
-    getAllPostsReducer
+    getAllPostsReducer,
+    getUserPostsReducer,
+    getCurrUserIdReducer
 })
