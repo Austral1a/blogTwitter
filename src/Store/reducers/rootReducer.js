ffb473613fa3f06efdecc9ffe8550c038c110119
Dan Korzh <lienglex@gmail.com>
2020-10-13 19:35:09 +0300

    add: added getAllPostsReducer to rootReducer

--- src/Store/reducers/rootReducer.js
+++ src/Store/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import getUsersReducer from './getUsersReducer'
+import getAllPostsActionCreator from '../actions/getAllPosts'
 
 export const rootReducer = combineReducers({
-    getUsersReducer
+    getUsersReducer,
+    getAllPostsActionCreator
 })
