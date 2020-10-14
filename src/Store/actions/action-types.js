638e2df7ed8381cc565b0bbe93c192970b6b54a2
Dan Korzh <lienglex@gmail.com>
2020-10-14 17:46:50 +0300

    add: UPD_POST action type

--- src/Store/actions/action-types.js
+++ src/Store/actions/action-types.js
@@ -3,3 +3,5 @@ export const GET_ALL_POSTS = 'GET_ALL_POSTS'
 export const GET_USER_POSTS = 'GET_USER_POSTS'
 export const GET_CURR_USER_ID = 'GET_CURR_USER_ID'
 export const GET_POST_COMMENTS = 'GET_POST_COMMENTS'
+// Update Post Operation
+export const UPD_POST = 'UPD_POST'
