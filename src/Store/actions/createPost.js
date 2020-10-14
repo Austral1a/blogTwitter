94f6ee8dd6209270729ec719c1faa7cdf39b2f77
Dan Korzh <lienglex@gmail.com>
2020-10-14 20:52:05 +0300

    add: createPost action

---
+++ src/Store/actions/createPost.js
@@ -0,0 +1,26 @@
+import {CREATE_POST} from './action-types'
+
+const createPostAction = (post) => ({
+    type: CREATE_POST,
+    post
+})
+
+const createPostActionCreator = (title, body, userId) => {
+    return (dispatch) => {
+        fetch(`https://jsonplaceholder.typicode.com/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title: title,
+                body: body,
+                userId: userId,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((res) => res.json())
+            .then((json) => dispatch(createPostAction(json)))
+    }
+}
+
+export default createPostActionCreator
