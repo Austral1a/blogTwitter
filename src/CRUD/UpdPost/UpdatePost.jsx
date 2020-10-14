802a933cae11822c2ee5e650394bf638496296e4
Dan Korzh <lienglex@gmail.com>
2020-10-14 17:51:58 +0300

    add: initial UpdatePost component

---
+++ src/CRUD/UpdPost/UpdatePost.jsx
@@ -0,0 +1,25 @@
+import React, {PureComponent} from 'react'
+
+
+class UpdatePost extends PureComponent {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isDrawerVisible: false,
+            newTitle: '',
+            newBody: ''
+        }
+    }
+
+
+    render() {
+        return('')
+    }
+
+
+}
+
+
+export default ConnectedUpdatePost
