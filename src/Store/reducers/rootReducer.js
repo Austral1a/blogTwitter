152ff5c88a01dc3f610d4d1d6656487a05da9e92
Dan Korzh <lienglex@gmail.com>
2020-10-15 00:34:13 +0300

    add: isUserSignedIn reducer to the rootReducer

--- src/Store/reducers/rootReducer.js
+++ src/Store/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import getAllPostsReducer from './getAllPostsReducer'
 import getUserPostsReducer from './getUserPostsReducer'
 import getCurrUserIdReducer from './getCurrUserIdReducer'
 import getPostCommentsReducer from './getPostCommentsReducer'
+import isUserSignedInReducer from './isUserSignedInReducer'
 
 export const rootReducer = combineReducers({
     getUsersReducer,
     getAllPostsReducer,
     getUserPostsReducer,
     getCurrUserIdReducer,
-    getPostCommentsReducer
+    getPostCommentsReducer,
+    isUserSignedInReducer
 })
