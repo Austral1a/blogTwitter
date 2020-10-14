2f0ea9d10cf3195324d47e68b0ca77720caefeb5
Dan Korzh <lienglex@gmail.com>
2020-10-14 22:30:31 +0300

    add: initial CreatePostDrawer

--- src/CRUD/Post/CreatePostDrawer.jsx
+++ src/CRUD/Post/CreatePostDrawer.jsx
@@ -1,7 +1,19 @@
 import React, {PureComponent} from 'react'
 
 class CreatePostDrawer extends PureComponent {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visible: false,
+            title: '',
+            body: ''
+        }
+    }
+
+
     render() {
-        return('')
+        return()
     }
 }
