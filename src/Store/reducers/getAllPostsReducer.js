022614e953b6c330162d806aab2f2787200a6498
Dan Korzh <lienglex@gmail.com>
2020-10-14 19:31:43 +0300

    add: added case when post is deleted from all posts

--- src/Store/reducers/getAllPostsReducer.js
+++ src/Store/reducers/getAllPostsReducer.js
@@ -1,4 +1,4 @@
-import {GET_ALL_POSTS} from '../actions/action-types'
+import {GET_ALL_POSTS, DEL_POST} from '../actions/action-types'
 
 const initState = {
     posts: {}
@@ -10,6 +10,10 @@ const getAllPostsReducer = (state = initState, action) => {
             return Object.assign({}, state, {
                 // action.posts gets its value from called corresponding
                 // action creator which in its turn invokes an action(GET_ALL_POSTS to the right of the `case` operator)
+                posts: action.posts
+            })
+        case DEL_POST:
+            return Object.assign({}, state, {
                 posts: action.posts
             })
         default:
