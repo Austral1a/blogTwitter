f7779bc1530dec8995c518bbc0d4a17e6796df3f
Dan Korzh <lienglex@gmail.com>
2020-10-14 19:30:35 +0300

    add: DEL_POST action type

--- src/Store/actions/action-types.js
+++ src/Store/actions/action-types.js
@@ -5,3 +5,5 @@ export const GET_CURR_USER_ID = 'GET_CURR_USER_ID'
 export const GET_POST_COMMENTS = 'GET_POST_COMMENTS'
 // Update Post Operation
 export const UPD_POST = 'UPD_POST'
+// Delete Post Operation
+export const DEL_POST = 'DEL_POST'
