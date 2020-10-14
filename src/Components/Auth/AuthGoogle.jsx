15f80d316f9cc0346dde46aad1cf5eaab29521c4
Dan Korzh <lienglex@gmail.com>
2020-10-15 00:09:19 +0300

    add: initial AuthGoogle component

---
+++ src/Components/Auth/AuthGoogle.jsx
@@ -0,0 +1,17 @@
+import React, {PureComponent} from 'react'
+import {connect} from 'react-redux'
+import {Button, Divider} from 'antd'
+import authWithGoogle from '../../Auth/authWithGoogle'
+import {GoogleOutlined} from '@ant-design/icons'
+import '../../styles/auth-container.scss'
+
+class AuthGoogle extends PureComponent {
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
