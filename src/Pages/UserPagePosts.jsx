42982d5180206efded9ed7ad8e298b250e7ce75e
Dan Korzh <lienglex@gmail.com>
2020-10-14 18:24:56 +0300

    add: UpdatePost as Button

--- src/Pages/UserPagePosts.jsx
+++ src/Pages/UserPagePosts.jsx
@@ -4,11 +4,13 @@ import {connect} from 'react-redux'
 import CommentsSection from '../CommentsSection'
 import getUserPostsActionCreator from '../Store/actions/getUserPosts'
 import getPostCommentsActionCreator from "../Store/actions/getPostComments";
+import UpdatePost from '../CRUD/UpdPost/UpdatePost'
 
 const mapStateToProps = (state) => ({
     posts: state.getUserPostsReducer.posts,
     currUserId: state.getCurrUserIdReducer.userId,
-    comments: state.getPostCommentsReducer.comments
+    comments: state.getPostCommentsReducer.comments,
+    updatedPost: state.updPostReducer.updatedPost
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -41,11 +43,24 @@ class UserPagePosts extends PureComponent {
                     {
                         Object.values(this.props.posts).map((post, id) => {
                             return (
-                                <Card
-                                    title={post.title}
+                                <Card       // Since resource will not be really updated on the server but it will be faked as if.
+                                            // I comparing updated post data with already existed one,
+                                            // and when existed post (title or body) are
+                                            // differ from updated post (title or body), title or body gets replace
+                                    title={this.props.updatedPost.id === post.id
+                                            && this.props.updatedPost.title
+                                            ? this.props.updatedPost.title : post.title}
                                     key={post.id + id}
+                                    extra={<UpdatePost
+                                        title={post.title}
+                                        body={post.body}
+                                        postId={post.id}
+                                    />}
                                 >
-                                    <p>{post.body}</p>
+                                    <p>{// the same as above but with body rather than title
+                                        this.props.updatedPost.id === post.id
+                                            && this.props.updatedPost.body
+                                            ? this.props.updatedPost.body : post.body}</p>
                                     <CommentsSection
                                         post={post}
                                         comments={this.props.comments}
