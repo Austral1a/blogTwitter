f5f574c75529aac9cc06682150564346701a6f1a
Dan Korzh <lienglex@gmail.com>
2020-10-14 20:06:15 +0300

    Added condition around post; Changed key value to 'post.title'

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,7 +5,8 @@ import {Link} from 'react-router-dom'
 import getAllPostsActionCreator from './Store/actions/getAllPosts'
 import getPostCommentsActionCreator from './Store/actions/getPostComments'
 import {Card} from 'antd'
-import UpdatePost from './CRUD/UpdPost/UpdatePost'
+import UpdatePost from './CRUD/Post/UpdatePost'
+
 
 import userNameByPostId from './helpers/userNameByPostId'
 
@@ -37,33 +38,36 @@ class Posts extends PureComponent {
                 {
                     Object.values(this.props.posts).map((post, id) => {
                         return (
-                            <Card
-                                    // Since resource will not be really updated on the server but it will be faked as if.
-                                    // I comparing updated post data with already existed one,
-                                    // and when existed post (title or body) are
-                                    // differ from updated post (title or body), title or body gets replace
-                                title={this.props.updatedPost.id === post.id
-                                        && this.props.updatedPost.title
-                                        ? this.props.updatedPost.title : post.title}
-                                key={post.id + id}
-                                extra={<UpdatePost
-                                    title={post.title}
-                                    body={post.body}
-                                    postId={post.id}
-                                />}
-                            >
-                                <p>Author: {<Link to={`/users/${post.userId}/posts`}>
-                                    {userNameByPostId(this.props.users, post.userId)}</Link>}
-                                </p>
-                                <p>{// the same as above but with body rather than title
-                                    this.props.updatedPost.id === post.id
-                                    && this.props.updatedPost.body
-                                    ? this.props.updatedPost.body : post.body}</p>
-                                <CommentsSection
-                                    post={post}
-                                    comments={this.props.comments}
-                                    onChange={() => this.props.getPostComments(post.id)} />
-                            </Card>
+                            post.id ?
+                                <Card
+                                        // Since resource will not be really updated on the server but it will be faked as if.
+                                        // I comparing updated post data with already existed one,
+                                        // and when existed post (title or body) are
+                                        // differ from updated post (title or body), title or body gets replace
+                                    title={this.props.updatedPost.id === post.id
+                                            && this.props.updatedPost.title
+                                            ? this.props.updatedPost.title : post.title}
+                                    key={post.title}
+                                    extra={<UpdatePost
+                                        title={post.title}
+                                        body={post.body}
+                                        posts={this.props.posts}
+                                        postId={post.id}
+                                    />}
+
+                                >
+                                    <p>Author: {<Link to={`/users/${post.userId}/posts`}>
+                                        {userNameByPostId(this.props.users, post.userId)}</Link>}
+                                    </p>
+                                    <p>{// the same as above but with body rather than title
+                                        this.props.updatedPost.id === post.id
+                                        && this.props.updatedPost.body
+                                        ? this.props.updatedPost.body : post.body}</p>
+                                    <CommentsSection
+                                        post={post}
+                                        comments={this.props.comments}
+                                        onChange={() => this.props.getPostComments(post.id)} />
+                                </Card> : null
                         )
                     })
                 }
