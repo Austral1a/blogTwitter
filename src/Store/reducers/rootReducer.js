16c0d5d9aa8b6486cadfc66d85a992263efc0655
Dan Korzh <lienglex@gmail.com>
2020-10-14 17:48:55 +0300

    add: added updPost reducer to rootReducer

--- src/Store/reducers/rootReducer.js
+++ src/Store/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import getAllPostsReducer from './getAllPostsReducer'
 import getUserPostsReducer from './getUserPostsReducer'
 import getCurrUserIdReducer from './getCurrUserIdReducer'
 import getPostCommentsReducer from './getPostCommentsReducer'
+import updPostReducer from './updPostReducer'
 
 export const rootReducer = combineReducers({
     getUsersReducer,
     getAllPostsReducer,
     getUserPostsReducer,
     getCurrUserIdReducer,
-    getPostCommentsReducer
+    getPostCommentsReducer,
+    updPostReducer
 })
