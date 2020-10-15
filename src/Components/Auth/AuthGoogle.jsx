f833ade5280045508b88d45fbacc3212f2c0366e
Dan Korzh <lienglex@gmail.com>
2020-10-15 13:23:28 +0300

    add: initial AuthGogle component

--- src/Components/Auth/AuthGoogle.jsx
+++ src/Components/Auth/AuthGoogle.jsx
@@ -1,36 +1,11 @@
 import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Button, Divider} from 'antd'
-import authWithGoogle from '../../Auth/authWithGoogle'
-import {GoogleOutlined} from '@ant-design/icons'
+
 import '../../styles/auth-container.scss'
 
-const mapDispatchToProps = (dispatch) => ({
-    authGoogle: () => {
-        dispatch(authWithGoogle)
-    }
-})
 
 class AuthGoogle extends PureComponent {
 
     render() {
-        return (
-            <>
-                <Divider />
-                <Button
-                    type='dashed'
-                    shape='circle'
-                    onClick={authWithGoogle}
-                    icon={<GoogleOutlined style={{color: '#1DA1F2', fontSize: '1.5em'}} />}
-                />
-            </>
-        )
+        return ()
     }
 }
-
-const ConnectedAuthGoogle = connect(
-    null,
-    mapDispatchToProps
-)(AuthGoogle)
-
-export default ConnectedAuthGoogle
