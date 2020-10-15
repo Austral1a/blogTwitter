6e024b779d1360b974a15c191828815ae4e00213
Dan Korzh <lienglex@gmail.com>
2020-10-15 13:23:53 +0300

    import modules

--- src/Components/Auth/AuthGoogle.jsx
+++ src/Components/Auth/AuthGoogle.jsx
@@ -1,6 +1,9 @@
 import React, {PureComponent} from 'react'
-
+import {connect} from 'react-redux'
+import {Button, Divider} from 'antd'
+import {GoogleOutlined} from '@ant-design/icons'
 import '../../styles/auth-container.scss'
+import createUserActionCreator from '../../Store/actions/createUser'
 
 
 class AuthGoogle extends PureComponent {
