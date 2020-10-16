fc3d8f1f066467db62efa8586d78aaa733a73e08
Dan Korzh <lienglex@gmail.com>
2020-10-16 19:25:06 +0300

    Added i18next provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ConnectedMainContainer from './MainContainer'
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
 import store from './Store/index'
+import {I18nextProvider} from 'react-i18next'
+import i18n from './i18n'
 import './styles/general.scss'
 import './styles/main-container.scss'
 
@@ -22,9 +24,11 @@ firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-        <ConnectedMainContainer />
-    </Provider>
+      <I18nextProvider i18n={i18n}>
+          <Provider store={store}>
+              <ConnectedMainContainer />
+          </Provider>
+      </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
