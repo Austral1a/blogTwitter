e99c38418ea16bffb8fddf1468e6bdcc47f6be44
Dan Korzh <lienglex@gmail.com>
2020-10-18 02:13:28 +0300

    Updated createUser action

--- src/Store/actions/createUser.js
+++ src/Store/actions/createUser.js
@@ -1,38 +1,43 @@
 import {CREATE_USER} from './action-types'
 import firebase from "firebase";
 import provider from "../../Auth/googleAuthProvider";
+import isUserEmailExists from '../../helpers/isUserEmailExists'
 
-const createUserAction = (users) => ({
+const createUserAction = (userId) => ({
     type: CREATE_USER,
-    users
+    userId
 })
 
-const createUserActionCreator = (users) => {
+const createUserActionCreator = () => {
     return (dispatch) => {
         firebase.auth().signInWithPopup(provider)
-            .then((res) => {
-                return res
-            })
+            .then((res) => res)
             .then((user) => {
-                let userName = user.additionalUserInfo.profile.name || `Unknown name ${users.length+1}`
+                let userName = user.additionalUserInfo.profile.name || `Unknown name`
                 let userEmail = user.additionalUserInfo.profile.email || 'Unknown email'
-                fetch('https://jsonplaceholder.typicode.com/users', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        id: users.length + 1,
-                        name: userName,
-                        email: userEmail,
-                        username: userName
-                    }),
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                    },
+                isUserEmailExists(userEmail).then(isExists => {
+                    // if the user's email is not in db, then create one, and set his id into the Store
+                    if(!isExists) {
+                        fetch('http://localhost:3000/users', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                name: userName,
+                                email: userEmail,
+                                username: userName
+                            }),
+                            headers: {
+                                'Content-type': 'application/json; charset=UTF-8',
+                            }
+                        })
+                            .then(res => res.json())
+                            .then(newUser => dispatch(createUserAction(newUser.id)))
+                    // if the user's email already exists in db, then only grab his id, and let him log in
+                    } else {
+                        fetch(`http://localhost:3000/users?email=${userEmail}`)
+                            .then(res => res.json())
+                            .then(existedUser => dispatch(createUserAction(existedUser[0].id)))
+                    }
                 })
-                    .then((response) => response.json())
-                    .then((json) => {
-                        console.log(json)
-                        dispatch(createUserAction(users.concat(json)))
-                    })
             })
     }
 }
