93ef6e124a1d8926f305b7f40f997f658293d508
Dan Korzh <lienglex@gmail.com>
2020-10-19 00:39:00 +0300

    Deleted useless prop

--- src/Components/Auth/AuthGoogle.jsx
+++ src/Components/Auth/AuthGoogle.jsx
@@ -12,8 +12,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    createUser: (users) => {
-        dispatch(createUserActionCreator(users))
+    createUser: () => {
+        dispatch(createUserActionCreator())
     }
 })
 
