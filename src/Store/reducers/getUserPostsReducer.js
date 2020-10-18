5e8859fb2d352cc3a8b423388903739e5ab09dc0
Dan Korzh <lienglex@gmail.com>
2020-10-18 14:58:28 +0300

    Removed DEL_POST case(it was extracted into delPostReducer)

--- src/Store/reducers/getUserPostsReducer.js
+++ src/Store/reducers/getUserPostsReducer.js
@@ -1,4 +1,4 @@
-import {CREATE_POST, DEL_POST, GET_USER_POSTS} from '../actions/action-types'
+import {CREATE_POST, GET_USER_POSTS} from '../actions/action-types'
 
 const initState = {
     posts: {}
@@ -10,10 +10,6 @@ const getUserPostsReducer = (state = initState, action) => {
             return Object.assign({}, state, {
                 // action.posts gets its value from called corresponding
                 // action creator which in its turn invokes an action(GET_USER_POSTS to the right of the `case` operator)
-                posts: action.posts
-            })
-        case DEL_POST:
-            return Object.assign({}, state, {
                 posts: action.posts
             })
         case CREATE_POST:
