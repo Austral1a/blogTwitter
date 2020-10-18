9489f37f42445b82f94ed80087f7d492aeb65381
Dan Korzh <lienglex@gmail.com>
2020-10-18 13:21:33 +0300

    Updated Post component, for correct posts rendering and to update it

--- src/Post.jsx
+++ src/Post.jsx
@@ -6,34 +6,25 @@ import PostToolbar from './CRUD/Post/PostToolbar'
 import {withTranslation} from 'react-i18next'
 import userNameByPostId from './helpers/userNameByPostId'
 
-const Post = ({posts, post, id, users, updatedPost, delPost, t}) => {
+const Post = ({posts, post, id, users, delPost, t}) => {
     return (
         post.id ?
             <Card
-                // Since resource will not be really updated on the server but it will be faked as if.
-                // I comparing updated post data with already existed one,
-                // and when existed post (title or body) are
-                // differ from updated post (title or body), title or body gets replace
-                title={updatedPost.id === post.id
-                && updatedPost.title
-                    ? updatedPost.title : post.title}
                 key={id + Math.random()}
-                extra={<PostToolbar
-                    title={post.title}
-                    body={post.body}
-                    posts={posts}
-                    postId={post.id}
-                    delPost={() => delPost(posts, post.id)}
-                />}
-
             >
-                <p>{t('postsSection.author')}: {<Link to={`/users/${post.userId}/posts`}>
-                    {userNameByPostId(users, post.userId)}</Link>}
-                </p>
-                <p>{// the same as above but with body rather than title
-                    updatedPost.id === post.id
-                    && updatedPost.body
-                        ?updatedPost.body : post.body}</p>
+                <h3>{post.title}</h3>
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <p>{t('postsSection.author')}: {<Link to={`/users/${post.userId}/posts`}>
+                        {userNameByPostId(users, post.userId)}</Link>}
+                    </p>
+                    <PostToolbar
+                        title={post.title}
+                        body={post.body}
+                        postId={post.id}
+                        delPost={() => delPost(posts, post.id)}
+                    />
+                </div>
+                <p>{post.body}</p>
                 <ConnectedCommentsSection
                     post={post}
                     postId={post.id} />
